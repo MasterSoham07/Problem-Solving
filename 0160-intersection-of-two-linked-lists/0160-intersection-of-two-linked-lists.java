1a92b3ee6c64fab5e346084d150397302a80f6cc
MasterSoham07 <167610299+MasterSoham07@users.noreply.github.com>
2024-04-22 22:35:46 +0530

    Time: 1 ms (99.97%), Space: 48.6 MB (35.73%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -34,36 +34,54 @@ public class Solution {
 
     // Approach 2
 
-    public ListNode collisionPoint(ListNode t1, ListNode t2, int d){
-        while(d != 0){
-            d--;
-            t2 = t2.next;
-        }
+    // public ListNode collisionPoint(ListNode t1, ListNode t2, int d){
+    //     while(d != 0){
+    //         d--;
+    //         t2 = t2.next;
+    //     }
 
-        while(t1 != t2){
-            t1 = t1.next;
-            t2 = t2.next;
-        }
+    //     while(t1 != t2){
+    //         t1 = t1.next;
+    //         t2 = t2.next;
+    //     }
 
-        return t1;
-    }
+    //     return t1;
+    // }
+
+    // public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+
+    //     ListNode temp1 = headA, temp2 = headB;
+
+    //     int n1 = 0, n2 =0;
+    //     while(temp1 != null){
+    //         n1++;
+    //         temp1 = temp1.next;
+    //     }
+
+    //     while(temp2 != null){
+    //         n2++;
+    //         temp2 = temp2.next;
+    //     }
+
+    //     if(n1 < n2) return collisionPoint(headA, headB, n2 - n1);
+    //     else return collisionPoint(headB, headA, n1 - n2);
+    // }
+
+    // Approach 3
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB){
-
         ListNode temp1 = headA, temp2 = headB;
 
-        int n1 = 0, n2 =0;
-        while(temp1 != null){
-            n1++;
+        while(temp1 != temp2){
             temp1 = temp1.next;
-        }
-
-        while(temp2 != null){
-            n2++;
             temp2 = temp2.next;
+
+            if(temp1 == temp2) return temp1;
+
+            if(temp1 == null) temp1 = headB;
+            if(temp2 == null) temp2 = headA;
         }
 
-        if(n1 < n2) return collisionPoint(headA, headB, n2 - n1);
-        else return collisionPoint(headB, headA, n1 - n2);
+        return temp1;
     }
 }
